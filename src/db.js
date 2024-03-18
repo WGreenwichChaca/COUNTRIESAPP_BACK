df9e50a4830579ba6e72f7464886c4e8d5ee8b7b
WGreenwichChaca <walter.greenwich@gmail.com>
2024-03-17 20:14:41 -0500

    Inicializar repositorio para el backend

--- src/db.js
+++ src/db.js
@@ -2,16 +2,11 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require('fs');
 const path = require('path');
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_DEPLOY } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
-const sequelize = new Sequelize(DB_DEPLOY, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
   logging: false,
   native: false,
-  dialectOptions: {
-    ssl: {
-      require: true,
-    }
-  }
 });
 
 const basename = path.basename(__filename);
